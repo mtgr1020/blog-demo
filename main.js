22b82d191d82dc25a53a56577610aa33660bffc5
mtgr <mtgr1020@gmail.com>
2020-07-08 10:39:29 +0800

    test Module

---
+++ main.js
@@ -0,0 +1,12 @@
+const instanceOf = (left, right) => {
+    let proto = left.__proto__;
+    let prototype = right.prototype;
+    while (true) {
+        if (proto === null) {
+            return false
+        } else if (proto === prototype) {
+            return true
+        }
+        proto = proto.__proto__
+    }
+}
